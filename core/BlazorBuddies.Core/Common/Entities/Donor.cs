9868330cafe7b2bfd6d37a49a824af669da52f6b
Jim Enright <apocdev@gmail.com>
2020-12-03 08:21:45 -0500

    chore: format code as per current solution formatting rules

--- core/BlazorBuddies.Core/Common/Entities/Donor.cs
+++ core/BlazorBuddies.Core/Common/Entities/Donor.cs
@@ -1,21 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace BlazorBuddies.Core.Common
 {
-    public class Donor
-    {
-        public Guid Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string EmailAddress { get; set; }
-        public string StreetAddress1 { get; set; }
-        public string StreetAddress2 { get; set; }
-        public string City { get; set; }
-        public string State { get; set; }
-        public string PostalCode { get; set; }
-        public Boolean OptOut { get; set; }
-        public string PhoneNumber { get; set; }
-    }
+	public class Donor
+	{
+		public Guid Id { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string EmailAddress { get; set; }
+		public string StreetAddress1 { get; set; }
+		public string StreetAddress2 { get; set; }
+		public string City { get; set; }
+		public string State { get; set; }
+		public string PostalCode { get; set; }
+		public bool OptOut { get; set; }
+		public string PhoneNumber { get; set; }
+	}
 }
--- web/BlazorBuddies.Web/Extensions/IApplicationBuilderExtensions.cs
+++ web/BlazorBuddies.Web/Extensions/IApplicationBuilderExtensions.cs
@@ -1,28 +1,33 @@
-﻿using BlazorBuddies.Core.Common;
+﻿using System;
+using System.Linq;
+
+using BlazorBuddies.Core.Common;
 using BlazorBuddies.Core.Data;
 using BlazorBuddies.Web.States;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace BlazorBuddies.Web.Extensions
 {
-	static internal class IApplicationBuilderExtensions
+	internal static class IApplicationBuilderExtensions
 	{
-		static internal IApplicationBuilder InitAppState(this IApplicationBuilder builder)
+		internal static IApplicationBuilder InitAppState(this IApplicationBuilder builder)
 		{
 			var state = builder.ApplicationServices.GetRequiredService<ApplicationState>();
 			var dbContextFactory = builder.ApplicationServices.GetRequiredService<IDbContextFactory<BuddyDbContext>>();
 			using var dbContext = dbContextFactory.CreateDbContext();
+
 #if DEBUG
+			// TODO: Move this to a proper seeding factory.
+			// https://docs.microsoft.com/en-us/ef/core/modeling/data-seeding
 			// Let's make sure our database is always up-to-date while developing
+
 			if (dbContext.Database.GetPendingMigrations().Any()) {
 				dbContext.Database.Migrate();
 			}
+
 			// Let's include at least one school with buddies while developing
 			if (!dbContext.Schools.Any()) {
 				dbContext.Schools.Add(new School {
@@ -34,6 +39,7 @@ namespace BlazorBuddies.Web.Extensions
 				dbContext.SaveChanges();
 			}
 #endif
+
 			state.BuddyCount = dbContext.Schools.Sum(s => s.Buddies);
 			return builder;
 		}
