06386b5f6dd0c78c3bea51b64d635e8d7eecd7cf
Jim Enright <apocdev@gmail.com>
2020-12-03 23:15:54 -0500

    feat(webhook): add dlr POST support

--- web/BlazorBuddies.Web/Controllers/Webhooks/DeliveryReceiptController.cs
+++ web/BlazorBuddies.Web/Controllers/Webhooks/DeliveryReceiptController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
 
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 using Vonage.Messaging;
@@ -9,6 +10,7 @@ namespace BlazorBuddies.Web.Controllers.Webhooks
 {
 	[Route("webhooks/dlr")]
 	[ApiController]
+	[AllowAnonymous] // TODO: Look into authenticated requests on these endpoints. We don't need random requests coming from unknown locations.
 	public class DeliveryReceiptController : ControllerBase
 	{
 		private readonly DonorService _donorService;
@@ -24,6 +26,17 @@ namespace BlazorBuddies.Web.Controllers.Webhooks
 		{
 			var dlr = WebhookParser.ParseQuery<DeliveryReceipt>(HttpContext.Request.Query);
 			return _donorService.HandleDlr(dlr);
+		}
+
+		[HttpPost]
+		public async Task PostAsync()
+		{
+			// Personally, I don't like just blindly passing the body of the request to be parsed here. It makes integration testing difficult.
+			// Newtonsoft JSON is also many many times slower than the new System.Text.JsonSerializer.
+			// However, this is what's documented in the Vonage SDK, so this is what we'll use.
+			// https://github.com/Vonage/vonage-dotnet-sdk#post-2
+			var dlr = await WebhookParser.ParseWebhookAsync<DeliveryReceipt>(Request.Body, Request.ContentType);
+			await _donorService.HandleDlr(dlr);
 		}
 	}
 }
